2e1e7801087c8ee9a3ea27a053ca6e4b518367bd
maettubfh <mdm5@bfh.ch>
2024-12-16 14:38:16 +0100

    return series, not dataframe, for assigning to new column

--- src/utils/transforms.py
+++ src/utils/transforms.py
@@ -8,7 +8,6 @@ def get_inspection_completeness(insp_appreciation):
         insp_appreciation.groupby([Appreciation.id_dossier])
         .agg({Appreciation.danger_level: lambda x: x.notna().sum()})
         .rename({Appreciation.danger_level: "Checked_Categories"}, axis=1)
-        .reset_index()
     )
     completeness = insp_completeness["Checked_Categories"] == 6
     return completeness
